4da1497747a34de5cda3fcc5a6be3f6634ab3b51
i-xiaohu <631751599@qq.com>
2026-08-19 19:18:59 +0800

    extract_centromere.py added.

---
+++ pairwise/raw_data/extract_centromere.py
@@ -0,0 +1,52 @@
+import gzip
+import sys
+
+from Bio import SeqIO
+
+
+def main(seq_fn: str, ann_fn: str, chr_name: str, out_fn: str):
+    cen_start, cen_end = -1, -1
+    max_active = 0
+    with open(ann_fn, 'r') as f:
+        for line in f:
+            cols = line.split()
+            chrom, start, end = cols[0], int(cols[1]), int(cols[2])
+            name, score = cols[3], int(cols[4])
+            rgb = [int(i) for i in cols[-1].split(',')]
+            # Pure red indicates active centromere regions
+            if chrom == chr_name and rgb[0] == 153 and rgb[1] == 0 and rgb[2] == 0:
+                print('Active region [%d,%d), name=%s, length=%d'
+                      % (start, end, name, end - start))
+                if end - start > max_active:
+                    max_active = end - start
+                    cen_start = start
+                    cen_end = end
+    if cen_end - cen_start == 0:
+        print('Found no centromere on %s' % chr_name)
+        return
+    print('Choose the active region [%d,%d) with max length %d' % (cen_start, cen_end, cen_end - cen_start))
+
+    with gzip.open(seq_fn, 'rt') as handle:
+        for record in SeqIO.parse(handle, "fasta"):
+            if record.id == chr_name:
+                print('Chromosome %s, length: %d, cen_ratio: %.2f' %
+                      (chr_name, len(record.seq), 100 * (cen_end - cen_start) / len(record.seq)))
+                with open(out_fn, 'w') as f:
+                    f.write('>cen_%s:%d_%d\n' % (chr_name, cen_start, cen_end))
+                    i = cen_start
+                    seg_len = 100
+                    while i + seg_len <= cen_end:
+                        segment = str(record.seq[i: i + seg_len]).upper()
+                        f.write('%s\n' % segment)
+                        i += seg_len
+                    if i < cen_end:
+                        segment = str(record.seq[i: cen_end]).upper()
+                        f.write('%s\n' % segment)
+                break
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print('Usage: extract_centromere.py <FASTA> <BED> <Chromosome> <Output>')
+        exit(1)
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
